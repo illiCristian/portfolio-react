7f4c32eba088ad575772abfef035190ce41bcde2
illiCristian <svrk73@gmail.com>
2023-01-29 10:38:14 -0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -11,15 +11,58 @@ const About = () => {
   return (
     <section ref={ref} id="about" className="section">
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* img */}
           <div
             className="flex-1 bg-about bg-contain bg-no-repeat h-[640px]
-          mix-blend-lighten bg-top">
-            img
-          </div>
+          mix-blend-lighten bg-top"></div>
           {/* text */}
-          <div>text</div>
+          <div className="flex-1">
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="h3 mb-4">I'am Freelance Front-end developer </h3>
+            <p className="mb-6">
+              lorem ipsoun dolor sit amet consectur adipisicng elit. dolor natus
+              omnis invetore quas beatea illo nulla
+            </p>
+            {/* stats */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Year of <br />
+                  Experience
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={15} duration={3} /> : null}
+                  k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Proojetcs <br />
+                  Completed
+                </div>
+              </div>
+              <div>
+                <div className="text-[40px] font font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={12} duration={3} /> : null}
+                  k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Satisfied <br />
+                  Clients
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-6 items-center">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
